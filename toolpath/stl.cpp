c341b44469b8cbdeba232d026ab08de1b135c828
Arjan van de Ven <arjanvandeven@gmail.com>
2020-02-01 18:17:50 -0800

    improve accuracy

--- toolpath/stl.cpp
+++ toolpath/stl.cpp
@@ -218,8 +218,10 @@ static inline double get_height_tool(double X, double Y, double R, bool ballnose
 	d = fmax(d, get_height(X - 0.7071 * R, Y - 0.7071 * R) + balloffset);
 	d = fmax(d, get_height(X + 0.7071 * R, Y - 0.7071 * R) + balloffset);
 
+#if 0
 	if (R < 0.6 && fabs(d-dorg) < 0.1)
 		return ceil(d*ACC)/ACC;
+#endif
 
 	d = fmax(d, get_height(X + 0.9239 * R, Y + 0.3827 * R) + balloffset);
 	d = fmax(d, get_height(X + 0.3827 * R, Y + 0.9239 * R) + balloffset);
@@ -481,7 +483,7 @@ static void create_toolpath(class scene *scene, int tool, bool roughing, bool ha
 				double d;
 				d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
 
-				if (fabs(d - last_Z) > 0.5 && roughing) {
+				if (fabs(d - last_Z) > 0.5) {
 					X = prevX + stepover / 3;
 					d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
 					if (fabs(d - last_Z) > 0.5) {
@@ -511,7 +513,7 @@ static void create_toolpath(class scene *scene, int tool, bool roughing, bool ha
 			while (X > -overshoot) {
 				double d;
 				d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
-				if (fabs(d - last_Z) > 0.5 && roughing) {
+				if (fabs(d - last_Z) > 0.5) {
 					X = prevX - stepover / 3;
 					d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
 					if (fabs(d - last_Z) > 0.5) {
@@ -552,7 +554,7 @@ static void create_toolpath(class scene *scene, int tool, bool roughing, bool ha
 			while (Y < maxY) {
 				double d;
 				d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
-				if (fabs(d - last_Z) > 0.5 && roughing) {
+				if (fabs(d - last_Z) > 0.5) {
 					Y = prevY + stepover / 3;
 					d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
 					if (fabs(d - last_Z) > 0.5) {
@@ -583,7 +585,7 @@ static void create_toolpath(class scene *scene, int tool, bool roughing, bool ha
 			while (Y > - overshoot) {
 				double d;
 				d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
-				if (fabs(d - last_Z) > 0.5 && roughing) {
+				if (fabs(d - last_Z) > 0.5) {
 					Y = prevY - stepover / 3;
 					d = get_height_tool(X, Y, radius + offset, ballnose) + offset - maxZ;
 					if (fabs(d - last_Z) > 0.5) {
