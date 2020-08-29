ee7eea5778cb0fdb38d6fae7e26392b124bcd41a
Arjan van de Ven <arjanvandeven@gmail.com>
2020-08-29 14:02:42 -0700

    gcodesplit application

---
+++ javascript/gcodesplit.js
@@ -0,0 +1,121 @@
+/*
+    Copyright (C) 2020 -- Arjan van de Ven
+    
+    Licensed under the terms of the GPL-3.0 license
+*/
+
+
+let filename = "output.nc";
+
+let filenames = []
+let filecontent = []
+
+
+function process_data(data)
+{
+    let lines = data.split("\n")
+    let len = lines.length;
+    
+    let toolchanges = 0;
+    let phase = 0
+    
+    let header = ""
+    let footer = ""
+    
+    console.log("Data is " + lines[0] + " size  " + lines.length);
+    
+    start = Date.now();
+    
+    console.log("Start of parsing at " + (Date.now() - start));
+
+    /* first a quick dryrun to find the header and footer text*/
+    dryrun = 1;    
+    for (let i = 0; i < len; i++) {
+    	line = lines[i];
+    	
+    	if (line == "M05") {
+    		phase = 1;
+    		toolchanges = toolchanges + 1;
+    		filecontent[toolchanges] = "";
+	}
+	if (line == "M02") {
+	        phase = 2;
+        }
+        
+        if (line.includes("M06") || line.includes("M6")) {
+            let index = line.indexOf("T");
+            let bit = "";
+            if (index >= 0) {
+                bit = line.substring(index + 1);
+                filenames[toolchanges] = toolchanges.toString() + "-" + bit + "-" + filename;
+            }
+                
+        }
+        
+        if (phase == 0) {
+            header = header + line + "\n";
+        }
+        
+        if (phase == 1) {
+             filecontent[toolchanges] = filecontent[toolchanges] + line + "\n";
+        }
+        
+        if (phase == 2) {
+            footer = footer + line + "\n";
+        }
+    }
+    
+    for (let i = 0; i <= toolchanges; i++) {
+            if (typeof(filenames[i]) == "undefined") {
+                continue;
+            };
+            var link = document.getElementById('download' + i.toString())
+            link.innerHTML = 'Download ' + filenames[i];
+            link.href = "#";
+            link.download = filenames[i];
+            link.href = "data:text/plain;base64," + btoa(header + filecontent[i] + footer);
+    }
+
+    console.log("Number of tool changes " + toolchanges);
+    console.log("End of parsing at " + (Date.now() - start));
+}
+
+
+function load(evt) 
+{
+    var start;
+    start =  Date.now();
+    if (evt.target.readyState == FileReader.DONE) {
+        process_data(evt.target.result); console.log("End of data processing " + (Date.now() - start));
+    }    
+}
+
+function basename(path) {
+     return path.replace(/.*\//, '');
+}
+
+function handle(e) 
+{
+    var files = this.files;
+    for (var i = 0, f; f = files[i]; i++) {
+    	fn = basename(f.name);
+    	if (fn != "" && fn.includes(".nc")) {
+    		filename = fn;
+    	}
+        var reader = new FileReader();
+        reader.onloadend = load;
+        reader.readAsText(f);
+    }
+}
+
+function Speedlimit(value)
+{
+    let newlimit = parseFloat(value);
+    
+    if (newlimit > 0 && newlimit < 500) {
+    	speedlimit = newlimit;
+    }
+}
+
+
+document.getElementById('files').onchange = handle;
