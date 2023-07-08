fbce484cb0e3c26b64f317ccc79e4a3350c85710
Arjan van de Ven <arjanvandeven@gmail.com>
2023-07-08 07:22:16 -0700

    have a threading option

--- inlaycheck/inlay.cpp
+++ inlaycheck/inlay.cpp
@@ -3,9 +3,38 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <pthread.h>
+
+
+class render *base, *plug;
+
+void *base_thread(void *arg)
+{
+    base = new render((const char *)arg);
+    base->load();
+    base->save_as_pgm("base.pgm");
+    
+    return NULL;
+}
+
+void *plug_thread(void *arg)
+{
+    plug =new render((const char *)arg);
+    plug->load();
+    plug->crop();
+    plug->save_as_pgm("plug2.pgm");
+    plug->cut_out();
+    plug->flip_over();
+    plug->save_as_pgm("plug.pgm");
+
+
+    return NULL;
+}
+
+
 int main(int argc, char **argv)
 {
-    class render *base, *plug;
+    pthread_t base_t, plug_t;
     
     double offset;
     
@@ -13,18 +42,17 @@ int main(int argc, char **argv)
         printf("Need 2 files as argument\n");
         exit(0);
     }
-        
-    base = new render(argv[1]);
-    base->load();
-    base->save_as_pgm("base.pgm");
-        
-    plug =new render(argv[2]);
-    plug->load();
-    plug->crop();
-    plug->cut_out();
-    plug->flip_over();
-    plug->save_as_pgm("plug.pgm");
 
+/*    
+    pthread_create(&base_t, NULL, base_thread, argv[1]);
+    pthread_create(&plug_t, NULL, plug_thread, argv[2]);
+        
+
+    pthread_join(base_t, NULL);        
+    pthread_join(plug_t, NULL);        
+*/
+    base_thread(argv[1]);
+    plug_thread(argv[2]);
 
     offset = find_best_correlation(base, plug);    
     
